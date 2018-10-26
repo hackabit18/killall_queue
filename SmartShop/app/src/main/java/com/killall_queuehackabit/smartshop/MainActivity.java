1f2b5dd6f05940f92e933a5525066d633c55c1d5
Abhishek Tiwari <abhishek.tiwari3507@gmail.com>
2018-10-27 00:05:54 +0530

    Initial commit

---
+++ SmartShop/app/src/main/java/com/killall_queuehackabit/smartshop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.killall_queuehackabit.smartshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
